af6934487f28534740d46453e2e9e2956c366332
akmal09 <akmal.119140203@student.itera.ac.id>
2023-08-19 08:42:14 +0700

    static side rendering

---
+++ src/pages/products/server.tsx
@@ -0,0 +1,35 @@
+import ProductView from "@/views/products"
+
+type ProductType = {
+    id:string;
+    name:string;
+    price:number;
+    image:string;
+    size:string;
+    category:string;
+}
+
+const ProductPage = (props : {productsData: ProductType[]})=>{
+    // ini harus sesuai dengan products data di bagian value parameter, misal key nya props dan value nya productData maka hook productData dapat di assign dengan variabel props
+    const {productsData} = props 
+
+    return(
+        <ProductView products={productsData} />
+    )
+}
+
+export default ProductPage
+
+// dipanggil setiap melakukan request
+export async function getServerSideProps() {
+    // fetch data
+    const res = await fetch("http://localhost:3000/api/product")
+    const responseData = await res.json()
+    // console.log(respon se)
+
+    return {
+        props:{
+            productsData: responseData.data
+        }
+    }
+}
